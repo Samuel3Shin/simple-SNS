3411520470303319eb8d0127121a22064b1c890c
Samuel3Shin <samuel3.shin@gmail.com>
2020-11-25 09:21:43 +0900

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+const Post= require('../models/post');
+const router = express.Router();
+
+router.post('/:id/follow', isLoggedIn, async(req, res, next) => {
+    try {
+        const user = await User.findOne({where: {id: req.user.id}});
+        if(user) {
+            await user.addFollowing(parseInt(req.params.id, 10));
+            res.send('success');
+        } else {
+            res.status(404).send('no user');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/:id/unfollow', isLoggedIn, async(req, res, next) => {
+    try {
+        const user = await User.findOne({where: {id: req.user.id}});
+        if(user) {
+            await user.removeFollowing(parseInt(req.params.id, 10));
+            res.send('success');
+        } else {
+            res.status(404).send('no user');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/:id/post-delete', isLoggedIn, async(req, res, next) => {
+    try {
+        console.log(req.body.value);
+
+        const item = await Post.findOne({
+            where: {
+                id: req.body.value,
+            }
+        })
+        await item.destroy();
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/:id/post-like', isLoggedIn, async(req, res, next) => {
+    try {
+        console.log(req.body.value);
+
+        const user = await User.findOne({where: {id: req.user.id}});
+        const post = await Post.findOne({
+            where: {
+                id: req.body.value,
+            }
+        })
+        user.addLikedPosts(post);
+        post.addLikedUsers(user);
+        console.log("like 성공했당!");
+
+        res.send('success');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/:id/post-unlike', isLoggedIn, async(req, res, next) => {
+    try {
+        console.log(req.body.value);
+
+        const user = await User.findOne({where: {id: req.user.id}});
+        const post = await Post.findOne({
+            where: {
+                id: req.body.value,
+            }
+        })
+        user.removeLikedPosts(post);
+        post.removeLikedUsers(user);
+        console.log("like 취소 성공했당!");
+
+        res.send('success');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+
+
+
+module.exports = router;
